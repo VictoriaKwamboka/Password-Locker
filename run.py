f823ce9104a959c26d4a1d2d581b0f0d5056bf00
VictoriaKwamboka <victoriamonari9815@gmail.com>
2022-04-25 14:08:44 +0300

    Add function and logic to run the password_locker function

--- run.py
+++ run.py
@@ -101,21 +101,120 @@ def copy_password(platform):
     return LogIn.copy_password(platform)
 
 
-def Password_locker():
+def Password_Locker():
     '''
     Define a function to run the Pasword locker
 
     '''
 
-    print("Hello Welcome to your Accounts Password Locker...\n Please enter one of the following to proceed.\n NEW ---  To Create A New Account  \n OLD ---  Already Have An Existing Account  \n")
+    print("Hello Welcome to your Password Locker...\n Please enter one of the following to proceed.\n NEW ---  To Create A New Account  \n OLD ---  Already Have An Existing Account  \n")
     short_code = input("").lower().strip()
 
     if short_code == 'new':
-        print('Sign Up for a new account:')
+        print('Sign up for a new account:')
         print('_' * 65)
 
-        username = input
+        username = input("What's your preferred username?" )
+        while True:
+            print(" TYPE - To type your preferred pasword:\n GEN - To generate a random password")
+            password_Choice = input().lower().strip()
+            if password_Choice == 'type':
+                password = input("Enter preferred password\n")
+                break
+            elif password_Choice == 'gen':
+                password = gen_password()
+                break
+            else:
+                print("Invalid short code. Please try again!")
+        save_user(add_new_user(username,password))
+        print("_" * 65)
+        print(f"Hello {username}, Your account has been created succesfully! Your password is: {password}")
+        print("_" * 65)
+    elif short_code == "old":
+        print("_" * 65)
+        print("Enter your username and password to log in:")
+        print('_' * 65)
+        username = input("What's your preferred username? ")
+        password = input("Enter password: ")
+        login = valid_user(username,password)
+        if valid_user == login:
+            print(f"Hello {username}. Welcome To The Password_Locker!")  
+            print('\n')
+    while True:
+        print("Use these short codes:\n CREATE - Create a new login details \n DISPLAY - Display log in details \n FIND - Find a log in detail for a platform \n GEN - Generate A random password \n DELETE- Delete log in details \n EXIT - Exit the application \n")
+        short_code = input().lower().strip()
+        if short_code == "create":
+            print("Create New Log Ins")
+            print("_"*65)
+            print("Platform Name? ")
+            platform = input().lower()
+            print("Your platform username: ")
+            username = input()
+            while True:
+                print(" TYPE - To type your own password if you already have an account:\n GEN - To generate random password")
+                password_Choice = input().lower().strip()
+                if password_Choice == 'type':
+                    password = input("Enter your preferred password\n")
+                    break
+                elif password_Choice == 'gen':
+                    password = gen_password()
+                    break
+                else:
+                    print("Invalid short code, please use 'type' or 'gen'")
+            save_new_login(add_new_login(platform,username,password))
+            print('\n')
+            print(f"Login Details for: {platform} are:  -Username: {username} - Password:{password} created succesfully")
+            print('\n')
+        elif short_code == "display":
+            if display_login_details():
+                print("Here's your list of saved platform log in details: ")
+                 
+                print('*' * 40)
+                print('_'* 30)
+                for platform in display_login_details():
+                    print(f" Account:{platform.platform} \n User Name:{username}\n Password:{password}")
+                    print('_'* 30)
+                print('*' * 30)
+            else:
+                print("You don't have any login details saved yet..........")
+        elif short_code == "find":
+            print("Enter the platform name you want to search: ")
+            search_name = input().lower()
+            if find_login(search_name):
+                search_login= find_login(search_name)
+                print(f"Account Name : {search_login.platform}")
+                print('-' * 65)
+                print(f"User Name: {search_login.username} Password :{search_login.password}")
+                print('-' * 65)
+            else:
+                print("That Credential does not exist")
+                print('\n')
+        elif short_code == "delete":
+            print("Enter the account name of the Credentials you want to delete")
+            search_name = input().lower()
+            if find_login(search_name):
+                search_login = find_login(search_name)
+                print("_"*65)
+                search_login.delete_login()
+                print('\n')
+                print(f"Your stored credentials for : {search_login.platform} successfully deleted!!!")
+                print('\n')
+            else:
+                print("That Credential you want to delete does not exist in your store yet")
+        elif short_code == 'gen':
+
+            password = gen_password()
+            print(f" {password} has been generated succesfull. You can proceed to use it to your platform!")
+        elif short_code == 'exit':
+            print("Thanks for using Password_Locker....Please pop by next time to use it again!")
+            break
+        else:
+            print("Wrong entry... Check your short code and let it match those in the menu! ")
+    else:
+        print("Please enter a valid input to continue")
+        
+            
     
 
 if __name__ == '__main__':
-    passlocker()
+    Password_Locker()
